305a8386889fd825567ec6c6fc82f01e6be2f237
Maru Ahues Bouza <mahues@mahues-macbookpro.roam.corp.google.com>
2013-11-26 17:34:27 -0800

    Changing conf parameters

--- config.py
+++ config.py
@@ -14,20 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-SERVICE_ACCOUNT_EMAIL_ADDRESS = '177146692044@developer.gserviceaccount.com'
-ISSUER_ID = '2956054058108947698'
+SERVICE_ACCOUNT_EMAIL_ADDRESS = 'ServiceAccountEmail@developer.gserviceaccount.com'
+ISSUER_ID = 'IssuerId'
 SERVICE_ACCOUNT_PRIVATE_KEY = 'wobs-privatekey.pem'
-APPLICATION_NAME = 'YOUR_APP_NAME'
+APPLICATION_NAME = 'ApplicationName'
 DISCOVERY_JSON = 'wobs-discovery.json'
-LOYALTY_CLASS_ID = 'LoyaltyClassPython1'
-LOYALTY_OBJECT_ID = 'LoyaltyObjectPython1'
+LOYALTY_CLASS_ID = 'LoyaltyClass'
+LOYALTY_OBJECT_ID = 'LoyaltyObject'
 OFFER_CLASS_ID = 'OfferClass'
 OFFER_OBJECT_ID = 'OfferObject'
 # List of origins for save to wallet button
 ORIGINS = [
-    'http://localhost:8080',
-    'http://wobs-quickstart-python.appspot.com',
-    'https://wobs-quickstart-python.appspot.com']
+    'http://localhost:8080']
 
 # Constants that are application agnostic.
 AUDIENCE = 'google'
